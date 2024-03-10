eaa4eb18baf6ee7997a89df922a723d30a7874e5
Menno Drescher <menno@cbaconsult.eu>
2024-03-10 17:15:37 +0100

    Microsoft Banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,21 +18,7 @@ export default function RootLayout({
         <Analytics />
         <SpeedInsights />
       </body>
-      <!-- Embed on page - Microsoft Authorized Refurbisher -->
-<div id="dccn-e64f665b-7b80-4f33-9b7b-b48a822418f3"></div>
-<script type='text/javascript'>
-    (function() {
-        const scriptId = 'e64f665b-7b80-4f33-9b7b-b48a822418f3';
-        const bannerHeight = 90; /* Override banner height */
-        const bannerWidth = 728; /* Override banner witdh */
-        const embedType = 'inline'; /* Set embed type: inline, banner */
 
-        const script = document.createElement('script');
-        script.defer = true;
-        script.src = `https://dmc.partner.microsoft.com/dccn/api/embedded-campaigns/js?id=${scriptId}&height=${bannerHeight}&width=${bannerWidth}&embedType=${embedType}`;
-        document.body.append(script);
-    })();
-</script>  
     </html>
   )
 }
