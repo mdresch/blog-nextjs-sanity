3b1d541a7e280e95100fd9893fec1fcc8079b9fe
Menno Drescher <menno@cbaconsult.eu>
2024-03-10 14:37:24 +0100

    First Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import 'tailwindcss/tailwind.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function RootLayout({
   children,
@@ -7,7 +8,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>Blog CBA Consult</title>
+          <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "leibon0hju");
+          </script>
+      </head>
+      <body>
+
+        {children}
+        <Analytics />
+      </body>
+      
     </html>
   )
 }
