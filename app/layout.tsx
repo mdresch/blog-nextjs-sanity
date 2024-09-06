2de4309d176718d328331dfca6ca225a16d71739
Menno Drescher <menno@cbaconsult.eu>
2024-09-06 10:56:20 +0200

    Commit Changes
    Commit changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import 'tailwindcss/tailwind.css';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import 'tailwindcss/tailwind.css'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function RootLayout({
   children,
@@ -9,17 +9,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
       <body>
-
         {children}
         <Analytics />
         <SpeedInsights />
-
-
       </body>
-
     </html>
   )
 }
-
